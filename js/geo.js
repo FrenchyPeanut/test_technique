f7475e909036400599aab421de8a40100137a839
Charles BEYER <charles.beyer.pro@gmail.com>
2017-01-06 12:13:39 +0100

    JS Update
    Postal Code now working

--- js/geo.js
+++ js/geo.js
@@ -5,18 +5,34 @@ function getCities() {
     //var data = ajax(url);
     var cities;
     $.getJSON(url, function(data) {
-    cities = data;
-    console.log(data);
-});
-    console.log(cities);
-    var object = jQuery.parseJSON(cities);
-  //  console.log(object);
+        cities = data;
+        console.log(data);
+        if (!data.results[0].postcode_localities) {
+            console.log(data.results[0].address_components[1].long_name);
+        }
+        else if (data.results[0].postcode_localities) {
+          //console.log(data.results[0].address_components[1].long_name);
+
+          $.each(data.results[0].postcode_localities, function(index,data){
+            console.log(data);
+          });
+
+          /*for(var i = 0; i < data.results[0].postcode_localities.lenght i++){
+          //  console.log(data.results[0].postcode_localities[i]);
+          console.log(i);
+        }*/
+        }
+        //if(!data.results[0].postcode_localities)
+    });
+    //console.log(cities);
+    //  console.log(object);
 
 }
 
+/*
 function ajax(url) {
     var cities = $.get(url,function( data ){
     },"json");
     console.log(cities);
     return cities;
-}
+}*/
